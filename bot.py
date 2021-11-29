12c43ab0282ab92438cf6c95b5e1c20fdce0bda3
Tindt <Tindt@KV2-PM2-1118>
2021-11-29 16:16:48 +0700

    create post message

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
+from typing import Text
 import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+env_path= Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client.chat_postMessage(channel='#learn-to-chat-new-world',text='Hi guy')
